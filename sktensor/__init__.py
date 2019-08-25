a1e920016917e0ef70eec43a5eeb4811e06658b6
Brian D. Holland <brian.d.holland@gmail.com>
2019-08-25 16:07:09 -0600

    fixed typo importing cp_apr

--- sktensor/__init__.py
+++ sktensor/__init__.py
@@ -5,11 +5,11 @@ from .core import *
 
 # data types
 from .sptensor import sptensor, unfolded_sptensor
-from .dtensor import dtensor, unfolded_dtensor
-from .ktensor import ktensor
+from .dtensor  import dtensor,  unfolded_dtensor
+from .ktensor  import ktensor
 
 # import algorithms
 from .cp import als as cp_als
-from .cp import als as cp_apr
+from .cp import apr as cp_apr
 from .tucker import hooi as tucker_hooi
 from .tucker import hooi as tucker_hosvd
